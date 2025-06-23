31ddaaff13f166b59ff44eb8ebb0ea35598d9b24
kian1991 <kianluetke@gmail.com>
2025-06-23 07:33:09 +0200

    feat: update CV_LINK to point to the correct Google Drive file

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 export const CV_LINK =
-  "https://drive.google.com/file/d/1kASRCQ4_yM7E8cXvxhgRh0Jyb2W04_ko/view?usp=sharing" as const;
+  "https://drive.google.com/file/d/1tHu4HUGi_MtLCPM-ul2t4bJO5gAshH8D/view?usp=sharing" as const;
 
 export const SOCIAL = {
   WHATS_APP: "https://wa.me/message/6JLKT55JL47QG1",
   LINKED_IN: "https://www.linkedin.com/in/kian-lütke-10a749188",
   GITHUB: "https://github.com/kian1991/",
   MAIL: "mailto:kianluetke@gmail.com",
-};
+} as const;
